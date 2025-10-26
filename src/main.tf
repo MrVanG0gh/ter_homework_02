9094a555cd9628a95e2f8722f5d255bad21df459
Vladimir Inshakov <mr_gogh@mail.ru>
2025-10-26 23:59:32 +0300

    Ex_1

--- src/main.tf
+++ src/main.tf
@@ -14,7 +14,7 @@ data "yandex_compute_image" "ubuntu" {
 }
 resource "yandex_compute_instance" "platform" {
   name        = "netology-develop-platform-web"
-  platform_id = "standart-v3"
+  platform_id = "standard-v3"
   resources {
     cores         = 2
     memory        = 1
