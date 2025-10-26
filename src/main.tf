ece7396f3464bffcbe5efb0dcfe9108d7527dc2c
Vladimir Inshakov <mr_gogh@mail.ru>
2025-10-27 02:18:20 +0300

    Ex_3

--- src/main.tf
+++ src/main.tf
@@ -11,7 +11,7 @@ resource "yandex_vpc_subnet" "develop" {
 data "yandex_compute_image" "ubuntu" {
   family          = var.family_name
 }
-resource "yandex_compute_instance" "platform_vm" {
+resource "yandex_compute_instance" "platform_web" {
   name            = var.vm_web_name
   platform_id     = var.vm_web_platform_id
   zone            = var.vm_web_zone
