f7f4cb7f04ebc2be1ba33214107e1fd514d2b837
Vladimir Inshakov <mr_gogh@mail.ru>
2025-10-27 01:37:49 +0300

    Ex_3

--- src/main.tf
+++ src/main.tf
@@ -3,7 +3,7 @@ resource "yandex_vpc_network" "develop" {
 }
 resource "yandex_vpc_subnet" "develop" {
   name            = var.vpc_name
-  # zone            = var.default_zone # уберем зону по умолчанию
+  zone            = var.default_zone
   network_id      = yandex_vpc_network.develop.id
   v4_cidr_blocks  = var.default_cidr
 }
