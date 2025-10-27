24a2fe1b0008394da663dd234706a9714a07d9bb
Vladimir Inshakov <mr_gogh@mail.ru>
2025-10-27 03:05:57 +0300

    Ex_4

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,7 +1,23 @@
-output "vm_web_output" {
-  value = yandex_compute_instance.platform_web.name && yandex_compute_instance.platform_web.network_interface[0].nat_ip_address && yandex_compute_instance.platform_web.fqdn
+output "vm_web_name" {
+  value = yandex_compute_instance.platform_web.name
 }
 
-output "vm_db_output" {
-  value = yandex_compute_instance.platform_db.name && yandex_compute_instance.platform_db.network_interface[0].nat_ip_address && yandex_compute_instance.platform_db.fqdn
+output "vm_web_ext_ip" {
+  value = yandex_compute_instance.platform_web.network_interface[0].nat_ip_address
+}
+
+output "vm_web_fqdn" {
+  value = yandex_compute_instance.platform_web.fqdn
+}
+
+output "vm_db_name" {
+  value = yandex_compute_instance.platform_db.name
+}
+
+output "vm_db_ext_ip" {
+  value = yandex_compute_instance.platform_db.network_interface[0].nat_ip_address
+}
+
+output "vm_db_fqdn" {
+  value = yandex_compute_instance.platform_db.fqdn
 }
