d05f943c5001bf63bc1fc05cd3db7dd2cfb1b639
Vladimir Inshakov <mr_gogh@mail.ru>
2025-10-27 04:27:39 +0300

    Final_fix_01

--- src/outputs.tf
+++ src/outputs.tf
@@ -4,7 +4,7 @@ output "VMs_output" {
     VM_web_FQDN   = yandex_compute_instance.platform_web.fqdn
     VM_web_ext_ip = yandex_compute_instance.platform_web.network_interface[0].nat_ip_address
     VM_db_name    = yandex_compute_instance.platform_db.name
-    VM_web_FQDN   = yandex_compute_instance.platform_db.fqdn
-    VM_web_ext_ip = yandex_compute_instance.platform_db.network_interface[0].nat_ip_address
+    VM_db_FQDN   = yandex_compute_instance.platform_db.fqdn
+    VM_db_ext_ip = yandex_compute_instance.platform_db.network_interface[0].nat_ip_address
   }
 }
